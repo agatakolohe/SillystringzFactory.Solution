0a3df663000bb231cc1686aeae4e71a63d379851
agatakolohe <agatakolohe@gmail.com>
2021-01-08 10:33:19 -0800

    create machine create route and view

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -19,5 +19,23 @@ namespace Factory.Controllers
             List<Machine> model = _db.Machines.ToList();
             return View(model);
         }
+
+        public ActionResult Create()
+        {
+            ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Machine machine, int EngineerId)
+        {
+            _db.Machines.Add(machine);
+            if (EngineerId != 0)
+            {
+                _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId });
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
