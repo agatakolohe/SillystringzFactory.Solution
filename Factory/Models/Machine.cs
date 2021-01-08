222baff119b68b08622aca1ec0627e5315fa8ce9
agatakolohe <agatakolohe@gmail.com>
2021-01-08 08:52:26 -0800

    create models

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,6 +6,15 @@ namespace Factory.Models
 {
     public class Machine
     {
+        public Machine()
+        {
+            this.Engineers = new HashSet<EngineerMachine>();
+        }
+
+        public int MachineId { get; set; }
+        public string MachineName { get; set; }
+        public string MachineDescription { get; set; }
+        public ICollection<EngineerMachine> Engineers { get; }
 
     }
 }
