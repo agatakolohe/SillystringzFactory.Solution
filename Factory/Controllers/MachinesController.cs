856c8bb4b5e3ea17a66bd0bb053bbb82fa96fec8
agatakolohe <agatakolohe@gmail.com>
2021-01-08 10:23:31 -0800

    add machine index route and view, edit engineer index view

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,23 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+    public class MachinesController : Controller
+    {
+        private readonly FactoryContext _db;
+        public MachinesController(FactoryContext db)
+        {
+            _db = db;
+        }
+        public ActionResult Index()
+        {
+            List<Machine> model = _db.Machines.ToList();
+            return View(model);
+        }
+    }
+}
